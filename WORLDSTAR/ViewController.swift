64ed187e99c4a8c01846cf69ed45e6c74b660a23
yoogchu <echu31@gatech.edu>
2017-12-30 13:11:35 -0600

    init

---
+++ WORLDSTAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WORLDSTAR
+//
+//  Created by Eugene Chu on 12/30/17.
+//  Copyright © 2017 peachy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

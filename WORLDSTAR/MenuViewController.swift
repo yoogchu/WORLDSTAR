9500f6939cf4e6050ca4eee234d0b44344c30eb2
yoogchu <echu31@gatech.edu>
2018-01-10 16:30:58 -0500

    collection view

---
+++ WORLDSTAR/MenuViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MenuViewController.swift
+//  WORLDSTAR
+//
+//  Created by Eugene Chu on 1/10/18.
+//  Copyright © 2018 peachy. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

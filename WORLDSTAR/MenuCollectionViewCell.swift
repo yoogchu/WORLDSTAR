da83dac90e6bf195d8913ff83d4a4e0ded323ea9
yoogchu <echu31@gatech.edu>
2018-01-10 22:01:56 -0500

    collection view cell setup

---
+++ WORLDSTAR/MenuCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCollectionViewCell.swift
+//  WORLDSTAR
+//
+//  Created by Eugene Chu on 1/10/18.
+//  Copyright © 2018 peachy. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var menuLabel: UILabel!
+    @IBOutlet weak var menuImage: UIImageView!
+}
---
+++ WORLDSTAR/menu_items.swift
@@ -0,0 +1,18 @@
+//
+//  menu_items.swift
+//  WORLDSTAR
+//
+//  Created by Eugene Chu on 1/10/18.
+//  Copyright © 2018 peachy. All rights reserved.
+//
+
+import Foundation
+
+enum items {
+    case today, calendar, statistics
+}
+struct menuItems {
+    let name: String
+    let image: 
+    enum menu
+}
